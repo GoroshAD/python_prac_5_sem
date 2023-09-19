01edd798e9dbefbd52cfb9d7e3f18935154c0cd9
Mr FRank <hahaha@gmail.com>
2023-09-19 14:14:30 +0300

    Been here, done here

--- 20230919/1/prog.py
+++ 20230919/1/prog.py
@@ -1,4 +1,7 @@
 x = eval(input())
 print("A", "+" if (x % 2 == 0 and x % 25 == 0) else "-", end = " ")
 print("B", "+" if (x % 2 == 1 and x % 25 == 0) else "-", end = " ")
-print("C", "+" if (x % 8 == 0) else "-")
+if x % 8 == 0 : 
+    print("C +") 
+else: 
+    print("C -")
