1e6e77c1e9c6aba1919b735e80f140ea650f3f5c
BuMetra <nikto8748@gmail.com>
2023-11-11 14:15:33 +0300

    Been at home, done there

---
+++ 20231107/3/prog.py
@@ -0,0 +1,24 @@
+class Undead(Exception) : pass
+class Skeleton(Undead) : pass
+class Zombie(Undead) : pass
+class Ghoul(Undead) : pass
+
+def necro(a) :
+    match (a % 3) :
+        case 0 :
+            raise Skeleton
+        case 1 :
+            raise Zombie
+        case _ :
+            raise Ghoul
+
+x, y = eval(input())
+for i in range(x, y) :
+    try :
+        necro(i)
+    except Skeleton :
+        print("Skeleton")
+    except Zombie :
+        print("Zombie")
+    except Undead :
+        print("Generic Undead")
